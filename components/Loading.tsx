cc328d90b3d2d6981106ee8db65195d937806626
Marcus Novoa <mnovcodes@gmail.com>
2021-02-26 01:54:13 -0500

    Update Loading screen spinner styles for sizing

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -3,13 +3,22 @@ import { StyleSheet, View } from 'react-native'
 import { Spinner, Text } from 'native-base'
 
 const Loading: React.FC = () => (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Spinner color='#fff' />
+    <View style={styles.container}>
+      <Spinner color='#fff' style={styles.spinner}/>
       <Text style={styles.text}>App Loading...</Text>
     </View>
 )
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  spinner: {
+    transform: [{ scaleX: 1.5 }, { scaleY: 1.5 }]
+  },
   text: {
     color: '#fff'
   }
